d467303b233ec52c56ded0350d6e2c3fc4c78924
unknown <lcanafistula.br@gmail.com>
2025-05-28 09:47:00 -0300

    ViewSets de Matricula, Matricula por Estudante e Matricula por curso

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
-from escola.serializers import EstudanteSerializer, CursoSerializer
-from escola.models import Estudante, Curso
+from rest_framework import viewsets, generics
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, MatriculaPorEstudanteSerializer, MatriculaPorCursoSerializer
+from escola.models import Estudante, Curso, Matricula
 
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
@@ -9,3 +9,19 @@ class EstudanteViewSet(viewsets.ModelViewSet):
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class MatriculaPorEstudante(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = MatriculaPorEstudanteSerializer
+
+class MatriculaPorCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = MatriculaPorCursoSerializer
