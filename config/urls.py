117c7aba2227b5e3e483a5bf0c109b048f8d4320
unknown <lcanafistula.br@gmail.com>
2025-05-28 09:50:40 -0300

    Rotas de Matricula

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from escola.views import EstudanteViewSet, CursoViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, MatriculaPorEstudante, MatriculaPorCurso
 
 router = routers.DefaultRouter()
 
 router.register('estudantes', EstudanteViewSet, basename='Estudantes')
 router.register('cursos', CursoViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('estudantes/<int:pk>/matriculas/', MatriculaPorEstudante.as_view()),
+    path('cursos/<int:pk>/matriculas/', MatriculaPorCurso.as_view())
 ]
