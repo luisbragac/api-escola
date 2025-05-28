593c9eaa94f1b1d88e72c05d920ea8f94a8af8a8
unknown <lcanafistula.br@gmail.com>
2025-05-28 09:14:42 -0300

    Models de Estudante e Curso

---
+++ escola/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Estudante(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150, blank=False)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+    numero_celular = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    NIVEL = [
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado')
+    ]
+
+    codigo = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=150)
+    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False, default='B')
+
+    def __str__(self):
+        return self.codigo
