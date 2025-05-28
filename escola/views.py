f9b319eef07935482e57ee71687c8d32fab915e1
unknown <lcanafistula.br@gmail.com>
2025-05-28 09:21:26 -0300

    ViewSet Estudante e Curso

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from escola.serializers import EstudanteSerializer, CursoSerializer
+from escola.models import Estudante, Curso
 
-# Create your views here.
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
